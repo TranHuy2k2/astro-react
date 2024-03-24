72a3abc04f85f4c4bc2856ad7b623a8372a39a5c
TranHuy2k2 <giahuytran2002@gmail.com>
2024-03-24 14:55:51 +0700

    chore: deploy to netifly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: netlify(),
   // Enable React to support React JSX components.
   integrations: [
     react(),
